678d0ddf60eee6022293e991c5e55743d386613b
phonk64 <phonk64@gmail.com>
2010-01-06 16:42:41 -0500

    corrected issue where nested content_for statements would not render all nested content

--- lib/session-view-helper.rb
+++ lib/session-view-helper.rb
@@ -1,9 +1,21 @@
 module SessionViewHelper
-  def display_session_hash
-  	"TEST!!!"
+  def display_session_hash()
+    if RAILS_ENV!='production'    	
+      concat(tag(:div, :id => "dev_container"))
+      concat(content_tag(:h2, 'Session Variables - Only renders in development mode')) 
+      session.collect {|name, content| concat(content_tag(:p, "#{name}: #{truncate(content.to_s, :length=>75)}", :id => "session_#{name}")) }           
+      concat('</div>')
+    end
   end
 end
 
 if defined?(ActionController)  
   ActionController::Base.helper(SessionViewHelper)
 end
+
+#<div id="dev_container">
+#  <h2>Session Variables - Only renders in development mode</h2>
+#  <% session.each do |name, content| %>
+#      <%= content_tag :p, "#{name}: #{truncate(content.to_s, :length=>75)}", :id => "session_#{name}" %>
+#    <% end %>
+#</div>
